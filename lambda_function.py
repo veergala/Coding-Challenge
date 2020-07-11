360a4c6860c4251be310632d20064a9cbb8301d7
Veer Gala <veergala@gmail.com>
2020-07-11 16:26:50 +0100

    Added MySQL functionality and connected to the veer_db database

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,14 @@
 import json
+import pymysql
+
+#Creates MySQL connection
+connection = pymysql.connect(host='internly-db.co952byay6uw.eu-west-2.rds.amazonaws.com',
+                             user='admin',
+                             password='tlc041!Durham',
+                             db='veer_db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+cursor = connection.cursor()
 
 def lambda_handler(event, context):
     # TODO implement
